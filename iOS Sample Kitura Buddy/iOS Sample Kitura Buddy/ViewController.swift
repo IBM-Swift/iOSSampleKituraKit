1f18379b58f0b0843a236e0c7f21670e6c8b43f8
ShihabMehboob <shihab.mehboob1@ibm.com>
2017-10-03 10:09:35 +0100

    Initial Commit
    Set up Xcode project.

---
+++ iOS Sample Kitura Buddy/iOS Sample Kitura Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Sample Kitura Buddy
+//
+//  Created by Shibab Mehboob on 03/10/2017.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
