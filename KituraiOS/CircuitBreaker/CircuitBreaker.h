60e28cf72a591974d683cabed60f0511db2f1e1a
Kye-Maloy <kye.maloy@ibm.com>
2017-10-17 11:20:42 +0100

    Updated icon, compiles locally

---
+++ KituraiOS/CircuitBreaker/CircuitBreaker.h
@@ -0,0 +1,19 @@
+//
+//  CircuitBreaker.h
+//  CircuitBreaker
+//
+//  Created by Kye Maloy on 17/10/2017.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircuitBreaker.
+FOUNDATION_EXPORT double CircuitBreakerVersionNumber;
+
+//! Project version string for CircuitBreaker.
+FOUNDATION_EXPORT const unsigned char CircuitBreakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircuitBreaker/PublicHeader.h>
+
+
---
+++ KituraiOS/LoggerAPI/LoggerAPI.h
@@ -0,0 +1,19 @@
+//
+//  LoggerAPI.h
+//  LoggerAPI
+//
+//  Created by Kye Maloy on 17/10/2017.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggerAPI.
+FOUNDATION_EXPORT double LoggerAPIVersionNumber;
+
+//! Project version string for LoggerAPI.
+FOUNDATION_EXPORT const unsigned char LoggerAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerAPI/PublicHeader.h>
+
+
