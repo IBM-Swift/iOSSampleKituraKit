657ae9b32470f261bee18a6eb3f336a5b1a08da6
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-27 22:51:06 -0500

    fixes to package.swift

--- ToDoServer/Package.swift
+++ ToDoServer/Package.swift
@@ -22,19 +22,16 @@ import PackageDescription
 let package = Package(
     name: "ToDoServer",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", .branch("issue.swift4")),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.0.0")),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
         .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMinor(from: "4.0.5")),
-        .package(url: "https://github.com/IBM-Swift/Configuration.git", .upToNextMinor(from: "1.0.0")),
-        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", .branch("swift4")),
+        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", .upToNextMinor(from: "2.0.0")),
         .package(url: "https://github.com/IBM-Swift/Health.git", from: "0.0.0"),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", .branch("master")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-CORS", .upToNextMinor(from: "1.7.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-CORS", .upToNextMinor(from: "2.0.0")),
         ],
     targets: [
         .target(name: "ToDoServer", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-        .target(name: "Application", dependencies: [ "KituraContracts", "Kitura", "KituraCORS", "Configuration", "CloudEnvironment", "Health" , "SwiftMetrics",
-         ]),
+        .target(name: "Application", dependencies: [ "Kitura", "KituraCORS", "CloudEnvironment", "Health" , "SwiftMetrics"]),
         ]
 )
 
